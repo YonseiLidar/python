98aa669e8dde450288f8ea641c7a0ffb888c3e69
SungchulLee <SungchulLee@users.noreply.github.com>
2020-10-12 22:36:59 +0900

    add

---
+++ import_module/package_1/sub_package_1/module_6.py
@@ -0,0 +1,10 @@
+var = 'module_6.py is imported!'
+print(var)
+
+def f(a, b):
+    print('Funstion f is Activated')
+    return a + b
+
+class Student:
+    def __init__(self):
+        print('Student Class Activated')
---
+++ import_module/package_2/module_10.py
@@ -0,0 +1,10 @@
+var = 'module_10.py is imported!'
+print(var)
+
+def f(a, b):
+    print('Funstion f is Activated')
+    return a + b
+
+class Student:
+    def __init__(self):
+        print('Student Class Activated')
